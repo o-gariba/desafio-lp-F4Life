3121d68bfece29b913d2601fc8ab5f06f19024ae
Pedro Garibaldi Santos <pedrogaribaldisantos@gmail.com>
2022-03-21 22:08:05 -0300

    validação do e-mail pelo js pronta.

--- validacao.js
+++ validacao.js
@@ -1,19 +1,30 @@
-const inputs = document.querySelectorAll('input');
+const botaoEnvio = document.querySelector('button');
 
-const padraoEmail = /^([a-zA-Z\d\.]{1,32})@([a-z\d]{1,16})$/;
+const padraoEmail = /^([a-zA-Z0-9\.]{1,32})@([a-z0-9]{1,16})\.(com)$/;
 //  Explicando o RegEx:
 //  a-zA-Z -> O user pode conter todas as letras do alfabeto, maiusculas e minusculas
 // \d -> permite numerais (digits)
 // \. -> permite pontos finais
 
 function validacao(campo, regEx) {
-    if(!(regEx.test(campo.value))) campo.className = 'input-invalido';
-    else campo.className = 'input-valido';
+    if(regEx.test(campo.value)) { 
+        campo.className = 'input-valido';
+        return true;
+    }
+
+    else {
+        campo.className = 'input-invalido';
+        return false;
+    }
 }
 
-inputs.forEach(input => {
-    input.addEventListener('keyup', evento => {
-        // console.log(evento.target.attributes.name.value);
-        validacao(evento.target, padraoEmail);
-    })
+botaoEnvio.addEventListener('click', (e) => {
+    const emailCadastrado = document.querySelector('input');
+
+    if (!(validacao(document.querySelector('input'), padraoEmail))) {
+        alert('e-mail inválido! Antes do @ aceitamos até 32 caracteres, incluindo numerais e ponto final. Depois do @ aceitamos até 16 caracteres minusculos e numerais. O final do seu e-mail deve terminar com ".com"');
+        e.preventDefault();
+    } else {
+        console.log('deu certo');
+    }
 })
